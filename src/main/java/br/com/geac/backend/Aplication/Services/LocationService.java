d67a044d3234a7adf869559b2e20918314c65ca1
Dimas Neto <dimasneto2015@gmail.com>
2026-02-24 14:58:55 -0300

    feat: Location CRUD

---
+++ src/main/java/br/com/geac/backend/Aplication/Services/LocationService.java
@@ -0,0 +1,70 @@
+package br.com.geac.backend.Aplication.Services;
+
+import br.com.geac.backend.Aplication.DTOs.Reponse.LocationResponseDTO;
+import br.com.geac.backend.Aplication.DTOs.Request.LocationPatchRequestDTO;
+import br.com.geac.backend.Aplication.DTOs.Request.LocationRequestDTO;
+import br.com.geac.backend.Aplication.Mappers.LocationMapper;
+import br.com.geac.backend.Repositories.LocationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+    private final LocationMapper locationMapper;
+
+    @Transactional
+    //TODO: constrain no banco para verificar unicidade
+    public LocationResponseDTO createLocation(LocationRequestDTO dto) {
+        if (locationRepository.existsByZipCodeAndNumberAndName(dto.zipCode(), dto.number(), dto.name())) { //TODO: Se precisar colocar referencepoint tbm
+            throw new RuntimeException("Location with the same zip code,number and name already exists");
+        }
+        var location = locationMapper.toEntity(dto);
+        return locationMapper.toDto(locationRepository.save(location));
+    }
+
+    public LocationResponseDTO getById(Integer id) {
+        var location = locationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Location not found"));
+        return locationMapper.toDto(location);
+    }
+
+    public List<LocationResponseDTO> getAll() {
+        return locationRepository.findAll().stream()
+                .map(locationMapper::toDto)
+                .toList();
+    }
+
+    @Transactional
+
+    public LocationResponseDTO updateLocation(Integer id, LocationPatchRequestDTO dto) {
+
+        var location = locationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Location not found"));
+
+        String newZipCode = dto.zipCode() != null ? dto.zipCode() : location.getZipCode();
+        String newNumber = dto.number() != null ? dto.number() : location.getNumber();
+        String newName = dto.name() != null ? dto.name() : location.getName();
+
+        if (locationRepository.existsByZipCodeAndNumberAndNameAndIdNot(newZipCode, newNumber, newName, id)) { //TODO: Se precisar colocar referencepoint tbm
+            throw new RuntimeException("Another location with the same zip code,number and name already exists");
+        }
+
+        locationMapper.updateEntityFromDTO(dto, location);
+
+        return locationMapper.toDto(locationRepository.save(location));
+    }
+
+    @Transactional
+    public void deleteLocation(Integer id) {
+
+        var location = locationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Location not found"));
+        locationRepository.delete(location); //TODO: aqui vai dar erro de integridade verificar como tratar
+    }
+}
